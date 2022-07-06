8932fa4c178024ed5c5e2fefb5b3c538650a322b
lisa <lisa.terashima72@gmail.com>
2022-07-06 19:15:09 +0900

    quizy内容こぴぺした

--- src/index.php
+++ src/index.php
@@ -1,1 +1,22 @@
-this is template
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Quizy</title>
+  <link rel="stylesheet" href="reset.css">
+  <link rel="stylesheet" href="quizy.css">
+</head>
+
+<body class="body">
+  <h1 class="subtitle">
+    ガチで東京の人しか解けない！ #東京の難読地名クイズ
+  </h1>
+  <div id="quizDivWrapper">
+  </div>
+  <script src="quizy.js" defer></script>
+</body>
+
+</html>
