53b80c284e40973aef154b3ce217a146a5662493
lisa <lisa.terashima72@gmail.com>
2022-11-12 19:21:40 +0900

    いったんぷっしゅ

--- src/connectdb.php
+++ src/connectdb.php
@@ -1,13 +1,11 @@
 <?php 
 
 try {
-
 // (1) データベースに接続
 $pdo = new PDO('mysql:charset=UTF8;dbname=posse;host=db', 'root', 'password');
-
+$pdo-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-
 // (2) エラーメッセージを出力
-echo $e->getMessage();
-
+echo '接続失敗' .  $e->getMessage();
+exit();
 } 
